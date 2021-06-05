0d4c4ac44b2e79efe8110c63f167a1839270106b
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2021-06-06 10:34:37 +1200

    Initial commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,5 @@
+<header class="section">
+  <h1 class="title has-text-primary">
+    Welcome to SvelteKit
+  </h1>
+</header>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from '$lib/Header.svelte'
+</script>
+
+<Header />
+
+<section class="section">
+  <h2 class="subtitle">
+    Coding made easy
+  </h2>
+
+  <p>
+    SvelteKit makes creating websites and web apps much easier!
+  </p>
+</section>
